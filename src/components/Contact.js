929d1b107b4ffc09f916d48e3fe17f6b009c9602
Laura Glutron <lauraglutron@MacBook-Pro.lan>
2020-10-15 13:32:39 +0200

    change false to props.online

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            online: false,
+            online: props.online,
         };
     }
     render(){
